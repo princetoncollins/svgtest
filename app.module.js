d398125d2201e70e4387758f35bb674b3402897e
Princeton Collins <collinsprinceton@gmail.com>
2021-02-20 12:36:40 -0700

    Working.

--- app.module.js
+++ app.module.js
@@ -1,15 +1,1 @@
-var app = angular.module('test', ['ui.router']);
-
-app.config(function($stateProvider, $urlRouterProvider) {
-
-// $urlRouterProvider.otherwise('/');
-
-    // $stateProvider
-
-    // .state('/', {
-    //     url: '/',
-    //             templateUrl: ''
-    //             controller: 'MainController'            
-    // })
-
-}); //End Config. OK
+var app = angular.module('test', []);
--- server.js
+++ server.js
@@ -1,25 +1,16 @@
 //Dependencies.
 var express = require('express');
-var router = express.Router();
 var bodyParser = require('body-parser');
-var cors = require('cors');
-var http = require('http');
-var path = require('path');
-var fs = require('fs');
 
 var port = 9001;
 
 var app = express();
 
-app.use(express.static(__dirname + ''));
+console.log(__dirname);
+
+app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
-
-
-//Connectivity.
-//_________________________________________________________________________________________________________________________
-
-// module.exports = router;
 
 app.listen(port, function() {
   console.log('Magic! Listening on port: ', port);
